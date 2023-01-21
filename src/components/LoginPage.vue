12c1faabf67ccc15d4f97fb64262537015a7efff
Brent Watkins <brent.n.watkins@gmail.com>
2023-01-21 11:06:47 -0700

    feature: Created basic Log In and Log Out pages

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+</script>
+
+<template>
+  <header>
+    <h1>Login</h1>
+    <h3>Please enter your username and password:</h3>
+  </header>
+  <body>
+    <div class="userinfo">
+      <v-text-field label="Username" v-model="username"></v-text-field>
+      <v-text-field label="Password" v-model="password"></v-text-field>
+    </div>
+    <div class="login">
+      <v-btn prepend-icon="mdi-login">Login</v-btn>
+      <p>Need an account?</p>
+      <v-btn prepend-icon="mdi-account-plus">Create Account</v-btn>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+body {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+header {
+  text-align: center;
+}
+.login {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  text-align: center;
+}
+.userinfo {
+  align-items: stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 50%;
+  min-width: 30%;
+}
+</style>
---
+++ src/components/LogoutPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>You have logged out.</h1>
+  </header>
+  <body>
+    <p>Click <a href="/#/">here</a> to return to the home page.</p>
+  </body>
+</template>
+
+<style scoped>
+body {
+  text-align: center;
+}
+header {
+  text-align: center;
+}
+</style>
