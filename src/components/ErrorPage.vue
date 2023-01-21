952770c13f19785527f2ba9936efb6135fe4de0e
Brent Watkins <brent.n.watkins@gmail.com>
2023-01-21 11:00:22 -0700

    feature: Added basic Error page

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>Oops!</h1>
+    <h3>Sorry, that page could not be found.</h3>
+  </header>
+  <body>
+    <p>Click <a href="/#/">here</a> to return to the home page.</p>
+  </body>
+</template>
+
+<style scoped>
+body {
+  text-align: center;
+}
+header {
+  text-align: center;
+}
+</style>
